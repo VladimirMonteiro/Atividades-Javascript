0827da847aec1370fbc1eb8ad850ef90ffa0d65b
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-09-15 08:27:55 -0300

    Fix: resolved push with invalid numbers

--- estruturaDados/array/atv1.js
+++ estruturaDados/array/atv1.js
@@ -3,9 +3,14 @@ const prompt = require("prompt-sync")();
 const array = [];
 
 for (let i = 0; i < 10; i++) {
-    let number = parseInt(prompt(`Informe ${i + 1} número: `));
+  let number = parseInt(prompt(`Informe ${i + 1} número: `));
+  if (!isNaN(number)) {
     array.push(number);
+  } else {
+    console.log("Número inválido, informe outro número: ");
+    i--;
+  }
 }
 
-console.log(`Sentido normal do array: [${array}]`)
-console.log(`Sentido inverso do array: [${array.reverse()}]`)
+console.log(`Sentido normal do array: [${array}]`);
+console.log(`Sentido inverso do array: [${array.reverse()}]`);
